096c33271e85d492f951cd3c83c3eea995c961e0
Reynard Kombucha <reynardk@in-d9.nsk.21-school.ru>
2022-07-12 14:19:42 +0700

    add swaps the n-th and m-th byte

--- bitwise_operations/main.c
+++ bitwise_operations/main.c
@@ -3,31 +3,31 @@
 #define LEN_BYT 8
 #define LEN_INT 4
 
-void print_bytes(int num);
-int get_byte(int num, int n);
-int insert_byte(int num, int n, int ins_byte);
+void print_bytes(int num); // prints all bytes of the number
+int get_byte(int num, int n); // gets n-th byte
+int insert_byte(int num, int n, int ins_byte); // inserts a byte in n-th place
+int change_bytes(int num, int n, int m); // swaps the n-th and m-th byte
 
 int main() {
     int num = 0xABCDEFBB;
     print_bytes(num);
     printf("\n");
-    int c = get_byte(num, 2);
-    num = insert_byte(num, 3, c);
+    num = change_bytes(num, 3, 1);
     print_bytes(num);
     return 0;
 }
 void print_bytes(int num) {
     for(int i = 0; i < LEN_INT; i++) {
         printf("%d byte is %X\n", i, (num & 0xFF)); // print last byte
-        num = num >> LEN_BYT; // shift one byte to the left
+        num = num >> LEN_BYT; // shift one byte to the right
     }
 }
 int get_byte(int num, int n) {
     return (num >> LEN_BYT*n) & 0xFF;
 }
 int insert_byte(int num, int n, int ins_byte) {
-    ins_byte = ins_byte << n*LEN_BYT;
-    switch(n) {
+    ins_byte = ins_byte << n*LEN_BYT;  // shift one byte to the left
+    switch(n) { // delete a byte in n-th place
         case 0:
             num = num & 0xFFFFFF00; break;
         case 1:
@@ -38,5 +38,15 @@ int insert_byte(int num, int n, int ins_byte) {
             num = num & 0x00FFFFFF; break;
     }
     num = num | ins_byte;
+    return num;
+}
+int change_bytes(int num, int n, int m) {
+    // check correct arguments
+    n = ((n >= 0) && (n < LEN_INT))*n + (n >= LEN_INT)*3;
+    m = ((m >= 0) && (m < LEN_INT))*m + (m >= LEN_INT)*3;
+    int byte_n = get_byte(num, n);
+    int byte_m = get_byte(num, m);
+    num = insert_byte(num, m, byte_n);
+    num = insert_byte(num, n, byte_m);
     return num;
 }
