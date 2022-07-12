cd5f9c4cb82147e3dcbb5151f463383e80ff2fcf
Reynard Kombucha <reynardk@in-d9.nsk.21-school.ru>
2022-07-12 13:22:38 +0700

    print all bytes of number

--- bitwise_operations/main.c
+++ bitwise_operations/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#define LEN_BYT 4
+
+void print_bytes(int num);
 int main() {
-    printf("Hello World\n");
+    int num = 0xABCDEFBB;
+    print_bytes(num);
     return 0;
+}
+void print_bytes(int num) {
+    for(int i = 0; i < LEN_BYT; i++) {
+        printf("%d byte is %X\n", (i + 1), (num & 0xFF)); // print last byte
+        num = num >> 8; // shift one byte to the left
+    }
 }
