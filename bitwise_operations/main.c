afcdf0f2d5745173e4adaf11b1a5a5a6fe80e21e
Reynard Kombucha <reynardk@in-d9.nsk.21-school.ru>
2022-07-12 14:25:24 +0700

    add fix swaps bytes

--- bitwise_operations/main.c
+++ bitwise_operations/main.c
@@ -6,13 +6,13 @@
 void print_bytes(int num); // prints all bytes of the number
 int get_byte(int num, int n); // gets n-th byte
 int insert_byte(int num, int n, int ins_byte); // inserts a byte in n-th place
-int change_bytes(int num, int n, int m); // swaps the n-th and m-th byte
+int swap_bytes(int num, int n, int m); // swaps the n-th and m-th byte
 
 int main() {
     int num = 0xABCDEFBB;
     print_bytes(num);
     printf("\n");
-    num = change_bytes(num, 3, 1);
+    num = swap_bytes(num, 3, 1);
     print_bytes(num);
     return 0;
 }
@@ -26,7 +26,7 @@ int get_byte(int num, int n) {
     return (num >> LEN_BYT*n) & 0xFF;
 }
 int insert_byte(int num, int n, int ins_byte) {
-    ins_byte = ins_byte << n*LEN_BYT;  // shift one byte to the left
+    ins_byte = ins_byte << n*LEN_BYT;  // shift n-th byte to the left
     switch(n) { // delete a byte in n-th place
         case 0:
             num = num & 0xFFFFFF00; break;
@@ -40,7 +40,7 @@ int insert_byte(int num, int n, int ins_byte) {
     num = num | ins_byte;
     return num;
 }
-int change_bytes(int num, int n, int m) {
+int swap_bytes(int num, int n, int m) {
     // check correct arguments
     n = ((n >= 0) && (n < LEN_INT))*n + (n >= LEN_INT)*3;
     m = ((m >= 0) && (m < LEN_INT))*m + (m >= LEN_INT)*3;
