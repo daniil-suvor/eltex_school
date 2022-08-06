c1be935874cb6f3fb6b12a26abcf3b4cafb76e3d
Reynard Kombucha <reynardk@ob-f1.nsk.21-school.ru>
2022-08-06 19:00:34 +0700

    dynamic library v1

--- calculator/menu.c
+++ calculator/menu.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
 #include "calculator.h"
 void check_scanf(int* a, int* b);
 void print_menu();
@@ -30,35 +32,49 @@ void free_buff() {
     while ((ch = getchar()) != '\n') {}
 }
 void menu() {
-    int a, b, comand;
-    while(comand != 5) {
-        print_menu();
-        if (scanf("%d", &comand) != 1) {
-            free_buff();
-            comand = 6;
-        }
-        switch(comand) {
-            case 1:
-                check_scanf(&a, &b);
-                printf("rezult is %d\n", sum(a, b));
-                break;
-            case 2:
-                check_scanf(&a, &b);
-                printf("rezult is %d\n", difference(a, b));
-                break;
-            case 3:
-                check_scanf(&a, &b);
-                printf("rezult is %d\n", product(a, b));
-                break;
-            case 4:
-                check_scanf(&a, &b);
-                printf("rezult is %d\n", fraction(a, b));
-                break;
-            case 5:
-                break;
-            default:
-                printf("Error comand!\n");
-                break;
-        }
-    }
+    void *sum_library;	// хандлер внешней библиотеки
+	int (*sum)(int, int);	// переменная для хранения адреса функции
+
+	//загрузка библиотеки
+	sum_library = dlopen("plagins/libsum.so", RTLD_LAZY);
+    if (!sum_library){
+		//если ошибка, то вывести ее на экран
+		fprintf(stderr, "dlopen() error: %s\n", dlerror());
+		exit(0);
+	}
+    sum = dlsym(sum_library, "sum");
+    int a, b; //, comand;
+    check_scanf(&a, &b);
+    printf("rezult is %d\n", sum(a, b));
+    // while(comand != 5) {
+    //     print_menu();
+    //     if (scanf("%d", &comand) != 1) {
+    //         free_buff();
+    //         comand = 6;
+    //     }
+    //     switch(comand) {
+    //         case 1:
+    //             check_scanf(&a, &b);
+    //             printf("rezult is %d\n", sum(a, b));
+    //             break;
+    //         case 2:
+    //             check_scanf(&a, &b);
+    //             printf("rezult is %d\n", difference(a, b));
+    //             break;
+    //         case 3:
+    //             check_scanf(&a, &b);
+    //             printf("rezult is %d\n", product(a, b));
+    //             break;
+    //         case 4:
+    //             check_scanf(&a, &b);
+    //             printf("rezult is %d\n", fraction(a, b));
+    //             break;
+    //         case 5:
+    //             break;
+    //         default:
+    //             printf("Error comand!\n");
+    //             break;
+    //     }
+    // }
+    dlclose(sum_library);
 }
